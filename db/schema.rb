00acdaf5d805f10108610153c8288aa35123da71
shin1rok <skssikm@gmail.com>
2019-05-28 07:07:55 +0900

    User追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_214152) do
+ActiveRecord::Schema.define(version: 2019_05_27_215135) do
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,4 +18,15 @@ ActiveRecord::Schema.define(version: 2019_05_27_214152) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "gender", default: 0, null: false
+    t.date "birthday"
+    t.bigint "prefecture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
+  end
+
+  add_foreign_key "users", "prefectures"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 require 'csv'
 
+User.destroy_all if User.exists?
 Prefecture.destroy_all if Prefecture.exists?
 prefectures_table = CSV.table('db/csv/prefectures.csv')
 prefectures_table.each do |row|
   Prefecture.create!(id:   row[:code],
                      name: row[:name])
 end
+
+100.times do |i|
+  User.create!(name:           "サンプルユーザ#{i}",
+               gender:         [0, 1, 2, 9].sample,
+               birthday:       Date.new(1990, 1,1).since(i.days),
+               prefecture_id: (1..47).to_a.sample)
+end
+
+
